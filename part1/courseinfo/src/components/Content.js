bce21a6b45ac274ae4690c13ea32751aec100a3c
letrananhvu <letrananhvu>
2020-06-30 21:14:51 +0300

    part1 1.2 - done

--- part1/courseinfo/src/components/Content.js
+++ part1/courseinfo/src/components/Content.js
@@ -1,18 +1,12 @@
 import React from "react";
-
+import Part from "./Part";
 const Content = ({part1, part2, part3, exercises1, exercises2, exercises3}) => {
   return (
-    <div>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-    </div>
+    <>
+      <Part name={part1} exercise={exercises1}/>
+      <Part name={part2} exercise={exercises2}/>
+      <Part name={part3} exercise={exercises3}/>
+    </>
   );
 };
 
