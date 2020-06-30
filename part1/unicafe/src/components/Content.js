9caa0d218591f37ccf79be275564e76ae59a5ece
letrananhvu <letrananhvu>
2020-06-30 21:49:27 +0300

    part1 1.6 - done

---
+++ part1/unicafe/src/components/Content.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Part from './Part'
+
+const Content = ({parts}) => {
+  return (
+    <>
+      {
+        parts.map((part,index) =>  <Part key={index} part={part}/>)
+      }
+    </>
+  )
+}
+
+export default Content
---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,38 @@
+import React, {useState} from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const feedback = (type) => {
+    if (type === 'good') {
+      setGood(good + 1)
+    } else if (type === 'neutral') {
+      setNeutral(neutral + 1)
+    } else {
+      setBad(bad + 1)
+    }
+  }
+  return (
+    <div>
+      <h1>give feedback</h1>
+
+      <button onClick={() => feedback('good')}>good</button>
+      <button onClick={() => feedback('neutral')}>neutral</button>
+      <button onClick={() => feedback('bad')}>bad</button>
+
+      <h2>statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+
+    </div>
+  )
+}
+
+ReactDOM.render(<App/>,
+  document.getElementById('root')
+)
