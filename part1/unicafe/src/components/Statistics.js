a2ab00d813fad5017bee6701f1b8673f1980e39d
letrananhvu <letrananhvu>
2020-06-30 22:03:51 +0300

    part1 1.9 - done

--- part1/unicafe/src/components/Statistics.js
+++ part1/unicafe/src/components/Statistics.js
@@ -4,13 +4,22 @@ const Statistics = ({good, neutral, bad}) => {
   return (
     <>
       <h2>statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      {good || neutral || bad ?
+        (
+          <>
+            <p>good {good}</p>
+            <p>neutral {neutral}</p>
+            <p>bad {bad}</p>
 
-      <p>all {good + neutral + bad}</p>
-      <p>average {(good - bad) / (good + neutral + bad)}</p>
-      <p>positive {good * 100 / (good + neutral + bad)} %</p>
+            <p>all {good + neutral + bad}</p>
+            <p>average {(good - bad) / (good + neutral + bad)}</p>
+            <p>positive {good * 100 / (good + neutral + bad)} %</p>
+          </>
+        )
+        : 'No feedback given'
+
+      }
+
     </>
   )
 }
