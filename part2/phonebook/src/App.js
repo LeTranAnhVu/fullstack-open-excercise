084fa9b632fd4b82b2f534703362c6da000719a5
letrananhvu <letrananhvu>
2020-07-01 15:28:38 +0300

    part2 2.8 - done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,36 +1,47 @@
 import React, {useState} from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([{name: 'Arto Hellas'}])
+  const [persons, setPersons] = useState([{name: 'Arto Hellas', number: '03-333-3232'}])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
-  const updateNewName = (e) => {
+  const updateNewValue = (e) => {
+    const fieldName = e.target.name
     const value = e.target.value
-    setNewName(value)
+    if (fieldName === 'name') {
+      setNewName(value)
+    } else if (fieldName === 'number') {
+      setNewNumber(value)
+    }
   }
   const updatePhoneBook = (e) => {
     e.preventDefault()
     const trimmedName = newName.trim()
-    if (trimmedName) {
+    const trimmedNumber = newNumber.trim()
+    if (trimmedName && trimmedNumber) {
       // prevent user input exist name
       const existedPerson = persons.find(person => person.name === trimmedName)
       if (existedPerson) {
         window.alert(`${trimmedName} is already added to phonebook`)
       } else {
         // allow to add
-        setPersons([...persons, {name: trimmedName}])
+        setPersons([...persons, {name: trimmedName, number: trimmedNumber}])
+
+        // reset input field
+        setNewName('')
+        setNewNumber('')
       }
     }
-
-    // reset input field
-    setNewName('')
   }
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input value={newName} type="text" onChange={updateNewName}/>
+          name: <input value={newName} type="text" name={'name'} onChange={updateNewValue}/>
+        </div>
+        <div>
+          number: <input value={newNumber} type="text" name={'number'} onChange={updateNewValue}/>
         </div>
         <div>
           <button onClick={updatePhoneBook} type="submit">add</button>
@@ -38,7 +49,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person) => <li key={person.name}>{person.name}</li>)}
+        {persons.map((person) => <li key={person.name}>{person.name} {person.number}</li>)}
       </ul>
     </div>
   )
