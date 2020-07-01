a771ed866e872ca554ddd4543b20313332512e2a
letrananhvu <letrananhvu>
2020-07-01 15:17:40 +0300

    part2 2.7 - done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -11,16 +11,26 @@ const App = () => {
   const updatePhoneBook = (e) => {
     e.preventDefault()
     const trimmedName = newName.trim()
-    if(trimmedName) {
-      setPersons([...persons, {name: trimmedName}])
+    if (trimmedName) {
+      // prevent user input exist name
+      const existedPerson = persons.find(person => person.name === trimmedName)
+      if (existedPerson) {
+        window.alert(`${trimmedName} is already added to phonebook`)
+      } else {
+        // allow to add
+        setPersons([...persons, {name: trimmedName}])
+      }
     }
+
+    // reset input field
+    setNewName('')
   }
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input type="text" onChange={updateNewName}/>
+          name: <input value={newName} type="text" onChange={updateNewName}/>
         </div>
         <div>
           <button onClick={updatePhoneBook} type="submit">add</button>
@@ -28,7 +38,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-      {persons.map((person) => <li key={person.name}>{person.name}</li>)}
+        {persons.map((person) => <li key={person.name}>{person.name}</li>)}
       </ul>
     </div>
   )
