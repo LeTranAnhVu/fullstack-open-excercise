248e669016cb67581ac96b10183a082ba9982faa
letrananhvu <letrananhvu>
2020-07-01 19:08:50 +0300

    part2 2.11 - done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,15 +1,11 @@
 import React, {useEffect, useState} from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    {name: 'Arto Hellas', number: '040-123456'},
-    {name: 'Ada Lovelace', number: '39-44-5323523'},
-    {name: 'Dan Abramov', number: '12-43-234345'},
-    {name: 'Mary Poppendieck', number: '39-23-6423122'}
-  ])
+  const [persons, setPersons] = useState([])
   const [displayPersons, setDisplayPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
@@ -27,6 +23,18 @@ const App = () => {
     }
     setSearchName(name)
   }
+
+  // fetch data from server
+  useEffect(() => {
+    const url = 'http://localhost:3001/persons'
+    axios.get(url).then(({data}) => {
+      if(data){
+        setPersons([...data])
+      }
+    }).catch(err => {
+      console.error(err)
+    })
+  },[])
 
   // whenever the person change such as push new person, then reset filter
   useEffect(() => {
