8f21f61e5f51b5289538eaf1a0cf58913ed602ad
letrananhvu <letrananhvu>
2020-07-02 22:07:01 +0300

    part3 3.10 - adapt baseUrl of backend

--- part2/phonebook/src/services/person.js
+++ part2/phonebook/src/services/person.js
@@ -1,15 +1,15 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = 'http://localhost:3001/api/persons'
 
 const getAll = () => axios.get(baseUrl)
 
-const create = (newPerson) => axios.post(baseUrl,newPerson)
+const create = (newPerson) => axios.post(baseUrl, newPerson)
 
 const deleteById = (id) => {
   return axios.delete(`${baseUrl}/${id}`)
 }
-const updateById = (id, newPerson) => axios.put(`${baseUrl}/${id}`,newPerson)
+const updateById = (id, newPerson) => axios.put(`${baseUrl}/${id}`, newPerson)
 
 export default {
   getAll,
