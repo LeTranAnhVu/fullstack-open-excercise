685d128018ca30a9069b18c853dc2a058ba24b5c
letrananhvu <letrananhvu>
2020-07-04 21:36:39 +0300

    part5 5.18, 5.19, 5.20, 5.21, 5.22 - done

--- part5/bloglist-frontend/cypress/support/commands.js
+++ part5/bloglist-frontend/cypress/support/commands.js
@@ -1,25 +1,30 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+const baseUrl = 'http://localhost:3003/api'
+
+Cypress.Commands.add('register', ({ username,name, password }) => {
+  cy.request('POST', `${baseUrl}/users`, {
+    username, password, name
+  })
+})
+
+Cypress.Commands.add('login', ({ username, password }) => {
+  cy.request('POST', `${baseUrl}/login`, {
+    username, password
+  }).then(({ body }) => {
+    localStorage.setItem('user', JSON.stringify(body))
+    localStorage.setItem('access_token', JSON.stringify(body.token))
+    cy.visit('http://localhost:3000')
+  })
+})
+
+Cypress.Commands.add('createBlog', ({ title, url, author }) => {
+  cy.request({
+    url: `${baseUrl}/blogs`,
+    method: 'POST',
+    body: { title, url, author },
+    headers: {
+      'authorization': `bearer ${JSON.parse(localStorage.getItem('access_token'))}`
+    }
+  })
+
+  cy.visit('http://localhost:3000')
+})
